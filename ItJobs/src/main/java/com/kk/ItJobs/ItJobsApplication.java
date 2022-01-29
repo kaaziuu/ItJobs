ea61907abf40a766b079b7866ab6cfbac10e4249
Karol Kaźmierczak <kaziu11511@gmail.com>
2022-01-29 15:14:08 +0100

    Init Commit

---
+++ ItJobs/src/main/java/com/kk/ItJobs/ItJobsApplication.java
@@ -0,0 +1,13 @@
+package com.kk.ItJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItJobsApplication.class, args);
+	}
+
+}
