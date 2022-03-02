40d5248a1ff57639e51f7f75b320f1f08e5ee6e2
Karol Kaźmierczak <kaziu11511@gmail.com>
2022-03-02 17:42:40 +0100

    FIX: Format date

--- JsApps/it-jobs/src/utils/common/DateFormat.ts
+++ JsApps/it-jobs/src/utils/common/DateFormat.ts
@@ -1,13 +1,22 @@
 const formatTime = (time: number): string => {
-    if (time < 9) {
+    if (time <= 9) {
         return `0${time}`;
     }
     return time.toString();
 };
 
+const formatDate = (date: number): string => {
+    if (date <= 9) {
+        return `0${date}`;
+    }
+    return date.toString();
+};
+
 const DateFormat = (date: Date, showTime?: boolean): string => {
     const newDate = new Date(date);
-    let result = `${newDate.getDate()}-${parseInt(newDate.getMonth().toString()) + 1}-${newDate.getFullYear()}`;
+    let result = `${formatDate(newDate.getDate())}-${formatDate(
+        parseInt(newDate.getMonth().toString()) + 1
+    )}-${newDate.getFullYear()}`;
     if (!!showTime) {
         result += ` ${formatTime(newDate.getHours())}:${formatTime(newDate.getMinutes())}`;
     }
